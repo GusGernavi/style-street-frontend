80f38a0ff33dc2307d0c2a0c41cdc76b925d3b93
Augusto Gernavi Menghini <augusto.gernavi@gmail.com>
2024-05-31 19:48:58 -0300

    Estrutura base do projeto

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "StyleStreet - Your style in your street",
+  description: "StyleStreet - ECommerce de exemplo",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
